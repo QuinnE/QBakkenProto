93bdbcf00d3aebe0ffe7e7419854ff767275a246
QuinnE <qcellis15@TBSLTMBA593.local>
2015-02-09 10:45:09 -0600

    Initial Commit

---
+++ BakkenPrototype/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BakkenPrototype
+//
+//  Created by qcellis15 on 2/9/15.
+//  Copyright (c) 2015 qcellis15. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BakkenPrototype/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BakkenPrototype
+//
+//  Created by qcellis15 on 2/9/15.
+//  Copyright (c) 2015 qcellis15. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
